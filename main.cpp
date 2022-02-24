2dadc8a29709170e78765fcfc98c2c356bab32c9
shin-841 <100315579+shin-841@users.noreply.github.com>
2022-02-24 15:44:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,92 @@
 #include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHServo.h>
+#include <FEHMotor.h>
+
+AnalogInputPin CdS(FEHIO::P3_0);
+FEHMotor leftWheel(FEHMotor::Motor0, 9.0);
+FEHMotor rightWheel(FEHMotor::Motor1, 9.0);
+AnalogInputPin right_opt(FEHIO::P2_0);
+AnalogInputPin middle_opt(FEHIO::P2_1);
+AnalogInputPin left_opt(FEHIO::P2_2);
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_1);
+
+enum LineStates {
+    MIDDLE,
+    RIGHT,
+    LEFT
+};
+
+void move_forward(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    rightWheel.SetPercent(percent);
+    leftWheel.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    rightWheel.Stop();
+    leftWheel.Stop();
+}
+
+void turn_right(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    //Set both motors to desired percent
+    //hint: set right motor backwards, left motor forwards
+
+    rightWheel.SetPercent(-25);
+    leftWheel.SetPercent(25);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    rightWheel.Stop();
+    leftWheel.Stop();
+}
+
+void turn_left(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    
+    //Set both motors to desired percent
+    rightWheel.SetPercent(25);
+    leftWheel.SetPercent(-25);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+
+    rightWheel.Stop();
+
+    leftWheel.Stop();
+}
 
 int main(void)
 {
     // Psuedocode
     // When light turns on, move forward 7 5/8 in
+    if (CdS.Value() > 0 && CdS.Value() < .6) {
+        move_forward();
+    }
     // Rotate left 45 degrees
     // Move forward 12 in
     // When light turns on, turn left 90 degrees
