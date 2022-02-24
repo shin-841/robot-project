a2f23bcec23f52e0d1671598e92c9ded529e2014
shin-841 <100315579+shin-841@users.noreply.github.com>
2022-02-24 12:54:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main(void)
 {
-    LCD.WriteLine("Hello World!");
-	return 0;
+    // Psuedocode
+    // When light turns on, move forward 7 5/8 in
+    // Rotate left 45 degrees
+    // Move forward 12 in
+    // When light turns on, turn left 90 degrees
+    // Based on the color of the light, turn left or right by 15 degrees
+    // When black line is found, follow the line and press the button
+    // Move back and turn 90 degrees left
+    // Move forward until behind left and turn left 90 degrees
+    // Move up until on the top
+    // Rotate 180 degrees and move down the ramp
 }
