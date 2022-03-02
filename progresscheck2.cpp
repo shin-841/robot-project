33096544ddff3ff6548a9e4ef40006fe6ce51414
shin-841 <100315579+shin-841@users.noreply.github.com>
2022-03-02 18:36:40 -0500

    Update progresscheck2.cpp

--- progresscheck2.cpp
+++ progresscheck2.cpp
@@ -12,6 +12,7 @@ AnalogInputPin middle_opt(FEHIO::P2_1);
 AnalogInputPin left_opt(FEHIO::P2_2);
 DigitalEncoder right_encoder(FEHIO::P0_0);
 DigitalEncoder left_encoder(FEHIO::P3_7);
+FEHServo servoForkLift(FEHServo::Servo7);
 
 enum LineStates {
     MIDDLE,
@@ -46,8 +47,8 @@ void turn_right(int percent, int counts) //using encoders
     //Set both motors to desired percent
     //hint: set right motor backwards, left motor forwards
 
-    rightWheel.SetPercent(-25);
-    leftWheel.SetPercent(25);
+    rightWheel.SetPercent(-1 * percent);
+    leftWheel.SetPercent(percent);
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
@@ -66,22 +67,41 @@ void turn_left(int percent, int counts) //using encoders
     left_encoder.ResetCounts();
     
     //Set both motors to desired percent
-    rightWheel.SetPercent(25);
-    leftWheel.SetPercent(-25);
+    rightWheel.SetPercent(percent);
+    leftWheel.SetPercent(-1 * percent);
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
 
     //Turn off motors
-
     rightWheel.Stop();
+    leftWheel.Stop();
+}
 
+void move_while_turning(int rightPercent, int leftPercent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    
+    //Set both motors to desired percent
+    rightWheel.SetPercent(rightPercent);
+    leftWheel.SetPercent(leftPercent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    rightWheel.Stop();
     leftWheel.Stop();
 }
 
 int main(void)
 {
+    servoForkLift.TouchCalibrate();
+    /*
     // Psuedocode
     int motor_percent = 25; //Input power level here
     int expected_counts = 43; //Input theoretical counts here (200)
@@ -90,17 +110,21 @@ int main(void)
     // When light turns on, move forward 7 5/8 in
     while (begin) {
         if(CdS.Value() < 1.0 && CdS.Value() > 0) {
-            move_forward(motor_percent, 5 * expected_counts);
+            move_forward(motor_percent, 7 * expected_counts + 10);
             begin = false;
         }
     }
     
     // Rotate right 45 degrees
-    turn_right(motor_percent, 70);
-    // Move forward 12 in
-    rightWheel.SetPercent(25);
-    leftWheel.SetPercent(25);
-    bool jukeboxValue = true;
-    move_forward(motor_percent, 12 * expected_counts);
-    turn_left();
+    turn_right(motor_percent, 45);
+    move_forward(motor_percent, 15 * expected_counts);
+    turn_left(motor_percent, 125);
+    move_forward(motor_percent, 4 * expected_counts + 5);
+    turn_left(motor_percent, 150);
+    move_forward(-1 * motor_percent, 2 * expected_counts + 10);
+    turn_right(motor_percent, 40);
+    move_forward(motor_percent, 3 * expected_counts);
+    move_forward(-1 * motor_percent, expected_counts);
+    turn_right(motor_percent, 30);
+    */
 }
